06f2f4d8b2b8c0855b360f9e94c11158a6f37a98
Artem <1992.s.a.a@gmail.com>
2022-12-02 14:16:58 +0300

    random

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
 ﻿Console.WriteLine("input your name, ");
-string username = WriteLine();
+string username = Console.ReadLine();
 Console.WriteLine("Hello!");
 Console.WriteLine(username);
+
+int numberA = 3;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
+
+int numberA = new Random().Next(1, 10); // 1, 2, 3 ... 9
+int numberB = new Random().Next(100, 1000); //100, 101 ... 999
+int result = numberB / numberA;
+Console.WriteLine(result);
