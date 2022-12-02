b8e9200e20fb2d67cafe60802e78bec31c26a652
Artem <1992.s.a.a@gmail.com>
2022-12-02 14:30:13 +0300

    Console WriteLine all results

--- Program.cs
+++ Program.cs
@@ -12,3 +12,10 @@ int numberC = new Random().Next(1, 10); // 1, 2, 3 ... 9
 int numberD = new Random().Next(1, 10); // 1, 2, 3 ... 9
 int result2 = numberD / numberC;
 Console.WriteLine(result2);
+
+int numberE = new Random().Next(1, 10);
+Console.WriteLine(numberE);
+int numberF = new Random().Next(1, 10);
+Console.WriteLine(numberF);
+int result3 = numberE + numberF;
+Console.WriteLine(result);
