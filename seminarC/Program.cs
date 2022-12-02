1cb0b5f4324a8becd1803a62776973e666733bf6
Artem <1992.s.a.a@gmail.com>
2022-12-02 20:18:45 +0300

    добавил отступы в блоках кода

--- seminarC/Program.cs
+++ seminarC/Program.cs
@@ -7,12 +7,16 @@ Console.WriteLine("Input number1 = ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input number2 = ");
 int number2 = Convert.ToInt32(Console.ReadLine());
+
 if(number1 < number2)
+
 {
     Console.WriteLine("Число 2 больше, чем число 1 :");
     Console.WriteLine(number2);
 }
+
 else
+
 {
     Console.WriteLine("Число 1 больше, чем число 2 :");
     Console.WriteLine(number1);
