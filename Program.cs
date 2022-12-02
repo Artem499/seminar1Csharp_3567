3eca7abe87f959e0a672964702751dd1d8717e0d
Artem <1992.s.a.a@gmail.com>
2022-12-02 14:32:54 +0300

    new commit

--- Program.cs
+++ Program.cs
@@ -13,9 +13,9 @@ int numberD = new Random().Next(1, 10); // 1, 2, 3 ... 9
 int result2 = numberD / numberC;
 Console.WriteLine(result2);
 
-int numberE = new Random().Next(1, 10);
+int numberE = new Random().Next(1, 10); // random 1 - 9
 Console.WriteLine(numberE);
-int numberF = new Random().Next(1, 10);
+int numberF = new Random().Next(1, 10); // random 1 - 9
 Console.WriteLine(numberF);
 int result3 = numberE + numberF;
 Console.WriteLine(result3);
