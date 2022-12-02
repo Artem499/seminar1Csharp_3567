6de01ab90a82c04382c4716cf47b103f6dde810f
Artem <1992.s.a.a@gmail.com>
2022-12-02 21:52:41 +0300

    первый тест программы

---
+++ seminarC3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+
+Console.WriteLine("input number : ");
+int intNumber= Convert.ToInt32(Console.ReadLine());
+
+if(intNumber / 2)
+{
+    Console.WriteLine("the number is divided by two : ");
+    Console.WriteLine(intNumber);
+}
+else
+{
+    Console.WriteLine("Error! the number is not divided by two")
+}
